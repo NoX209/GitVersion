e80fa0014a04e16df3801790cc63c45638107814
NoX <86572462+NoX209@users.noreply.github.com>
2022-11-27 13:48:01 +0100

    output build infos on startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
+#include <build_infos.h> // will be created while building the project
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("setup");
+  Serial.printf(
+    "(VERSION:%s) (SHA:%s) (DATE:%s)\n",
+    Build.getGitVersion(),
+    Build.getGitHash(),
+    Build.getBuildTime()
+  );
 }
 
 #define seconds *1000
